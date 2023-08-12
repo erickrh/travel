fad511fb3d3c745bf41054d2fa04476ab445b0d3
Erick <123erickrh@gmail.com>
2023-08-12 10:47:15 -0500

    Add tailwind.config.js settings

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,19 @@ function App() {
 
   return (
     <>
-      <p>Hello</p>
+    <div className="font-Montserrat text-4xl text-center">Hola</div>
+
+      <div className='grid gap-2 grid-cols-3 grid-rows-3 bg-primary w-96 h-auto p-5'>
+        <div className="w-1/9 h-16 bg-blue-300 flex justify-center items-center">1</div>
+        <div className="w-1/9 h-16 bg-blue-300 flex justify-center items-center">2</div>
+        <div className="w-1/9 h-16 bg-blue-300 flex justify-center items-center">3</div>
+        <div className="w-1/9 h-16 bg-blue-300 flex justify-center items-center">4</div>
+        <div className="w-1/9 h-16 bg-blue-300 flex justify-center items-center">5</div>
+        <div className="w-1/9 h-16 bg-blue-300 flex justify-center items-center">6</div>
+        <div className="w-1/9 h-16 bg-blue-300 flex justify-center items-center">7</div>
+        <div className="w-1/9 h-16 bg-blue-300 flex justify-center items-center">8</div>
+        <div className="w-1/9 h-16 bg-blue-300 flex justify-center items-center">9</div>
+      </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,32 @@ export default {
     "./src/**/*.{js,jsx,}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'sanFrancisco': "url('.src/img/sanFrancisco.jpg')",
+        'sanFranciscoDesktop': "url('.src/img/sanFranciscoDesktop.jpg')",
+        'yosemite': "url('.src/img/yosemite.jpg')",
+        'LA': "url('.src/img/LA.jpg')",
+        'seattle': "url('.src/img/seattle.jpg')",
+        'new_york': "url('.src/img/new_york.jpg')",
+        'norway': "url('.src/img/norway.jpg')",
+        'sydney': "url('.src/img/sydney.jpg')",
+        'miami': "url('.src/img/miami.jpg')",
+        'switzerland': "url('.src/img/switzerland.jpg')",
+        'bali': "url('.src/img/bali.jpg')",
+        'chicago': "url('.src/img/chicago.jpg')",
+        'europe': "url('.src/img/europe.jpg')",
+        'iceland': "url('.src/img/iceland.jpg')",
+        },
+        colors: {
+          'primary':"#CC2D4A",
+          'secondary':"#8FA206",
+          'tertiary':"#61AEC9",
+        },
+        fontFamily: { 
+          Montserrat: ["Montserrat", "sans-serif"],
+        },
+    },
   },
   plugins: [],
 }
